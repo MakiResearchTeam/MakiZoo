e54d31c8cea59bfabade8c55e9a0eba0a3cc479c
TaplierShiru <45129308+TaplierShiru@users.noreply.github.com>
2020-03-12 19:38:31 +0400

    fix(backbones/vgg/__init__): fix imports

--- makizoo/backbones/vgg/__init__.py
+++ makizoo/backbones/vgg/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from .models import VGG16, VGG19
 from .blocks import repeat_n_convLayers
 from .builder import build_VGG
-from .utils import get_batchnorm_params, get_maxpool_params
+from .utils import get_batchnorm_params, get_pool_params
 
 del absolute_import
 
